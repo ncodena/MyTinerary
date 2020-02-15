4900e631dc4e8d3594992eff96399923f14907f1
Nuria <c.odena.nuria@gmail.com>
2020-02-15 12:56:28 +0100

    navbar component created

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    Container,
+    NavLink,
+    }from 'reactstrap' ;
+
+class AppNavbar extends Component {
+
+    state = {
+        isOpen: false
+    }
+
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="sm">
+                    <Container>
+                        <NavbarBrand href="/">MYTINERARY <i class="fas fa-globe-europe"></i></NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="/">
+                                        Link
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+
+                    </Container>
+
+                </Navbar>
+            
+                
+            </div>
+        )
+    }
+}
+
+export default AppNavbar; 

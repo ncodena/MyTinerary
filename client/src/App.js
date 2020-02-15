20ff76f3094122cfee7f9cb91716a4a09d9b9f83
Nuria <c.odena.nuria@gmail.com>
2020-02-15 13:28:29 +0100

    footer component and style

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar/>
+      <Footer/>
     </div>
   );
 }

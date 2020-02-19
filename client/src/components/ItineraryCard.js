10da7012cee50f37dc3a540c8e895c3d4e86aa82
Nuria <c.odena.nuria@gmail.com>
2020-02-19 16:35:37 +0100

    itinerary card component displayed

--- client/src/components/ItineraryCard.js
+++ client/src/components/ItineraryCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Fragment} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -14,6 +15,7 @@ import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import EuroIcon from '@material-ui/icons/Euro';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,7 +27,6 @@ const useStyles = makeStyles(theme => ({
   },
   expand: {
     transform: 'rotate(0deg)',
-    marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
@@ -47,12 +48,31 @@ export default function ItineraryCard(props) {
   console.log(itinerary)
 
 
-
-  const [expanded, setExpanded] = React.useState(false);
+  const [isOpen, setOpen] = React.useState(false);
 
   const handleExpandClick = () => {
-    setExpanded(!expanded);
+    setOpen(!isOpen);
   };
+
+  const displayActivities = (props) => {
+
+
+    const {itinerary} = props
+
+    console.log(itinerary)
+
+    if(setOpen) {
+      return (
+        itinerary.activities.map (activity => {
+          return (
+            <Fragment>
+              <p>{activity.title}</p>
+              <h1>Hi</h1>
+            </Fragment>
+          )})
+      )
+    }
+  }
 
   return (
     <Card className={classes.root}>
@@ -68,7 +88,7 @@ export default function ItineraryCard(props) {
           </IconButton>
         }
         title={itinerary.title}
-        subheader="September 14, 2016"
+        subheader={itinerary.duration}
       />
       <CardMedia
         className={classes.media}
@@ -77,52 +97,32 @@ export default function ItineraryCard(props) {
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          {itinerary.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
-        <IconButton
+      </CardActions>
+
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        <CardContent>
+        <h1>hi</h1>
+          {displayActivities(props)} 
+        </CardContent>
+      </Collapse>
+
+      <IconButton
           className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
+            [classes.expandOpen]: isOpen,
           })}
           onClick={handleExpandClick}
-          aria-expanded={expanded}
           aria-label="show more"
         >
           <ExpandMoreIcon />
         </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
+
     </Card>
   );
 }

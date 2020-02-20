f16822c52041f92cd221a51518d2691cc0e97d73
Nuria <c.odena.nuria@gmail.com>
2020-02-20 17:41:06 +0100

    auth reducer displayed and integrated

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,51 @@
+import {USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL} from '../actions/types';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    currentUser: {},
+    isLoading: false,
+    // user: null,
+    favourites: []
+};
+
+export default function(state = initialState, action) {
+    switch(action.type){
+        case USER_LOADING:
+            return {
+               ...state,
+               isLoading: true,
+            };
+        case USER_LOADED:
+            return {
+                ...state,
+                isLoading: false,
+                currentUser: action.currentUser,
+                isAuthenticated: true,
+                favourites: action.favourites
+            };
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+                ...action.payload,
+                isLoading: false,
+                isAuthenticated: true,
+                favourites: action.favourites
+            };
+        case REGISTER_FAIL:
+        case LOGIN_FAIL:
+        case AUTH_ERROR:
+        case LOGOUT_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                currentUser: {},
+                token: null,
+                favourites:[]
+            };
+        default:
+            return state
+            
+    }
+}

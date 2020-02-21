9196ff5fc534330844e61c4baa571c80ed5db8f9
Nuria <c.odena.nuria@gmail.com>
2020-02-21 14:40:45 +0100

    building register form

--- client/src/views/RegisterModal.js
+++ client/src/views/RegisterModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, Input, Label, Form, FormGroup } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, Input, Label, Form, FormGroup, NavLink } from 'reactstrap';
 import { connect } from 'react-redux';
 
 
@@ -7,16 +7,273 @@ import { connect } from 'react-redux';
     class RegisterModal extends Component {
 
         state = {
-            firstName,
-            lastName, 
-            userName, 
-            password,
-            email,
-            country,
-            img,
-            hasAgreed
+            modal: false,
+            firstName: '',
+            lastName: '', 
+            userName: '',
+            password: '',
+            email: '',
+            country: '',
+            hasAgreed: false,
+            msg: null,
 
-        }
+            countryList : [
+                "Afghanistan",
+                "Albania",
+                "Algeria",
+                "American Samoa",
+                "Andorra",
+                "Angola",
+                "Anguilla",
+                "Antarctica",
+                "Antigua and Barbuda",
+                "Argentina",
+                "Armenia",
+                "Aruba",
+                "Australia",
+                "Austria",
+                "Azerbaijan",
+                "Bahamas (the)",
+                "Bahrain",
+                "Bangladesh",
+                "Barbados",
+                "Belarus",
+                "Belgium",
+                "Belize",
+                "Benin",
+                "Bermuda",
+                "Bhutan",
+                "Bolivia (Plurinational State of)",
+                "Bonaire, Sint Eustatius and Saba",
+                "Bosnia and Herzegovina",
+                "Botswana",
+                "Bouvet Island",
+                "Brazil",
+                "British Indian Ocean Territory (the)",
+                "Brunei Darussalam",
+                "Bulgaria",
+                "Burkina Faso",
+                "Burundi",
+                "Cabo Verde",
+                "Cambodia",
+                "Cameroon",
+                "Canada",
+                "Cayman Islands (the)",
+                "Central African Republic (the)",
+                "Chad",
+                "Chile",
+                "China",
+                "Christmas Island",
+                "Cocos (Keeling) Islands (the)",
+                "Colombia",
+                "Comoros (the)",
+                "Congo (the Democratic Republic of the)",
+                "Congo (the)",
+                "Cook Islands (the)",
+                "Costa Rica",
+                "Croatia",
+                "Cuba",
+                "Curaçao",
+                "Cyprus",
+                "Czechia",
+                "Côte d'Ivoire",
+                "Denmark",
+                "Djibouti",
+                "Dominica",
+                "Dominican Republic (the)",
+                "Ecuador",
+                "Egypt",
+                "El Salvador",
+                "Equatorial Guinea",
+                "Eritrea",
+                "Estonia",
+                "Eswatini",
+                "Ethiopia",
+                "Falkland Islands (the) [Malvinas]",
+                "Faroe Islands (the)",
+                "Fiji",
+                "Finland",
+                "France",
+                "French Guiana",
+                "French Polynesia",
+                "French Southern Territories (the)",
+                "Gabon",
+                "Gambia (the)",
+                "Georgia",
+                "Germany",
+                "Ghana",
+                "Gibraltar",
+                "Greece",
+                "Greenland",
+                "Grenada",
+                "Guadeloupe",
+                "Guam",
+                "Guatemala",
+                "Guernsey",
+                "Guinea",
+                "Guinea-Bissau",
+                "Guyana",
+                "Haiti",
+                "Heard Island and McDonald Islands",
+                "Holy See (the)",
+                "Honduras",
+                "Hong Kong",
+                "Hungary",
+                "Iceland",
+                "India",
+                "Indonesia",
+                "Iran (Islamic Republic of)",
+                "Iraq",
+                "Ireland",
+                "Isle of Man",
+                "Israel",
+                "Italy",
+                "Jamaica",
+                "Japan",
+                "Jersey",
+                "Jordan",
+                "Kazakhstan",
+                "Kenya",
+                "Kiribati",
+                "Korea (the Democratic People's Republic of)",
+                "Korea (the Republic of)",
+                "Kuwait",
+                "Kyrgyzstan",
+                "Lao People's Democratic Republic (the)",
+                "Latvia",
+                "Lebanon",
+                "Lesotho",
+                "Liberia",
+                "Libya",
+                "Liechtenstein",
+                "Lithuania",
+                "Luxembourg",
+                "Macao",
+                "Madagascar",
+                "Malawi",
+                "Malaysia",
+                "Maldives",
+                "Mali",
+                "Malta",
+                "Marshall Islands (the)",
+                "Martinique",
+                "Mauritania",
+                "Mauritius",
+                "Mayotte",
+                "Mexico",
+                "Micronesia (Federated States of)",
+                "Moldova (the Republic of)",
+                "Monaco",
+                "Mongolia",
+                "Montenegro",
+                "Montserrat",
+                "Morocco",
+                "Mozambique",
+                "Myanmar",
+                "Namibia",
+                "Nauru",
+                "Nepal",
+                "Netherlands (the)",
+                "New Caledonia",
+                "New Zealand",
+                "Nicaragua",
+                "Niger (the)",
+                "Nigeria",
+                "Niue",
+                "Norfolk Island",
+                "Northern Mariana Islands (the)",
+                "Norway",
+                "Oman",
+                "Pakistan",
+                "Palau",
+                "Palestine, State of",
+                "Panama",
+                "Papua New Guinea",
+                "Paraguay",
+                "Peru",
+                "Philippines (the)",
+                "Pitcairn",
+                "Poland",
+                "Portugal",
+                "Puerto Rico",
+                "Qatar",
+                "Republic of North Macedonia",
+                "Romania",
+                "Russian Federation (the)",
+                "Rwanda",
+                "Réunion",
+                "Saint Barthélemy",
+                "Saint Helena, Ascension and Tristan da Cunha",
+                "Saint Kitts and Nevis",
+                "Saint Lucia",
+                "Saint Martin (French part)",
+                "Saint Pierre and Miquelon",
+                "Saint Vincent and the Grenadines",
+                "Samoa",
+                "San Marino",
+                "Sao Tome and Principe",
+                "Saudi Arabia",
+                "Senegal",
+                "Serbia",
+                "Seychelles",
+                "Sierra Leone",
+                "Singapore",
+                "Sint Maarten (Dutch part)",
+                "Slovakia",
+                "Slovenia",
+                "Solomon Islands",
+                "Somalia",
+                "South Africa",
+                "South Georgia and the South Sandwich Islands",
+                "South Sudan",
+                "Spain",
+                "Sri Lanka",
+                "Sudan (the)",
+                "Suriname",
+                "Svalbard and Jan Mayen",
+                "Sweden",
+                "Switzerland",
+                "Syrian Arab Republic",
+                "Taiwan (Province of China)",
+                "Tajikistan",
+                "Tanzania, United Republic of",
+                "Thailand",
+                "Timor-Leste",
+                "Togo",
+                "Tokelau",
+                "Tonga",
+                "Trinidad and Tobago",
+                "Tunisia",
+                "Turkey",
+                "Turkmenistan",
+                "Turks and Caicos Islands (the)",
+                "Tuvalu",
+                "Uganda",
+                "Ukraine",
+                "United Arab Emirates (the)",
+                "United Kingdom of Great Britain and Northern Ireland (the)",
+                "United States Minor Outlying Islands (the)",
+                "United States of America (the)",
+                "Uruguay",
+                "Uzbekistan",
+                "Vanuatu",
+                "Venezuela (Bolivarian Republic of)",
+                "Viet Nam",
+                "Virgin Islands (British)",
+                "Virgin Islands (U.S.)",
+                "Wallis and Futuna",
+                "Western Sahara",
+                "Yemen",
+                "Zambia",
+                "Zimbabwe",
+                "Åland Islands"
+            ]
+
+
+
+
+
+        };
 
         toggle = () => {
             //Clear errors
@@ -39,9 +296,13 @@ import { connect } from 'react-redux';
             //Create user object
     
             const newUser = {
-                name,
-                email,
+                firstName,
+                lastName, 
+                userName, 
                 password,
+                email,
+                country,
+                hasAgreed,
             }
     
             //Attempt to register
@@ -52,36 +313,56 @@ import { connect } from 'react-redux';
             //Close modal
             // this.toggle();
         }
+
+        chooseCountry() {
+    
+            return this.state.countryList.map((country, index) =>{
+
+                <option value= {country} key={index}>{country}</option>
+
+            })
+         }
     
         
     render() {
     return (
         <div>
-            <Form inline onSubmit={(e)=> e.preventDefault()}>
-                <FormGroup>
-                    <Label for="backdrop">Backdrop value</Label>{' '}
-                    <Input type="select" name="backdrop" id="backdrop" onChange={changeBackdrop}>
-                    <option value="true">true</option>
-                    <option value="false">false</option>
-                    <option value="static">"static"</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup className="mx-2" check>
-                    <Label check>
-                        <Input type="checkbox" checked={keyboard} onChange={changeKeyboard} /> Keyboard
-                    </Label>
-                </FormGroup>
-                {' '}
-                <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
-            </Form>
-            <Modal isOpen={modal} toggle={toggle} className={className} backdrop={backdrop} keyboard={keyboard}>
-                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <NavLink onClick={this.toggle} href="#">
+                Register
+            </NavLink>
+            <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggle}>Registration Form</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                    anim id est laborum.
+                    <Form onSubmit={this.onSubmit}>  
+                    <FormGroup>
+
+                        <Label for="firstName">First Name</Label>
+                        <Input type="text" name="firstName" id="firstName" placeholder="First Name" onChange={this.onChange}/>
+
+                        <Label for="lastName">Last Name</Label>
+                        <Input type="text" name="lastName" id="lastName" placeholder="Last Name" onChange={this.onChange}/>
+
+                        <Label for="userName">User Name</Label>
+                        <Input type="text" name="userName" id="userName" placeholder="UserName" onChange={this.onChange}/>
+
+                        <Label for="email">E-mail address</Label>
+                        <Input type="text" name="email" id="email" placeholder="E-mail" onChange={this.onChange}/>
+
+                        <Label for="country">Country</Label>
+                        <Input type="select" name="country" id="country" onChange={this.onChange}>
+                            <option value="false">Choose your country</option>
+                            {this.chooseCountry()}
+                        </Input>
+            </FormGroup>
+            <FormGroup className="mx-2" check>  
+            <Label check>
+                <Input type="checkbox" checked={keyboard} onChange={changeKeyboard} /> Keyboard
+            </Label>
+            </FormGroup>
+            {' '}
+            <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+        </Form>
+                    
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
